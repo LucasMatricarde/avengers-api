f3a40535b61d23b4985d04098884d9c2623fe174
Lucas Matricarde <lucas.eduardo@wises.com.br>
2024-11-18 20:04:41 -0300

    primeiro commit

---
+++ src/test/java/lucasmatricarde/com/avengersapi/AvengersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lucasmatricarde.com.avengersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvengersApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
